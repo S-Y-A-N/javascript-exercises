83a74a925fa109a856bb8203c8e0e1143f0ea98a
SYAN <syanays.git@gmail.com>
2023-08-25 15:26:25 +0300

    remove old code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,4 @@
 const removeFromArray = function(array, ...removeElements) {
-    
-    // for(let i = 0; i < array.length; i++) {
-    //     for(let j = 0; j < removeElements.length; j++) {
-    //         if(array[i] === removeElements[j]) {
-    //             array.splice(i, 1);
-                
-    //         }
-    //     }
-    // }
 
     for(let i = 0; i < removeElements.length; i++) {
         for(let j = 0; j < array.length; j++) {
