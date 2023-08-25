c48f05ef20b500ee8c27d4b30cd39063d161e0ee
SYAN <syanays.git@gmail.com>
2023-08-25 15:25:24 +0300

    Add solution, test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeElements) {
+    
+    // for(let i = 0; i < array.length; i++) {
+    //     for(let j = 0; j < removeElements.length; j++) {
+    //         if(array[i] === removeElements[j]) {
+    //             array.splice(i, 1);
+                
+    //         }
+    //     }
+    // }
 
+    for(let i = 0; i < removeElements.length; i++) {
+        for(let j = 0; j < array.length; j++) {
+            if(removeElements[i] === array[j]) {
+                array.splice(j, 1);
+                
+            }
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
