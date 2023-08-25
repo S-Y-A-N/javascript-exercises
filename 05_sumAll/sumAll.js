26cc12161bb436771c6fef93cd240e1a80917453
SYAN <syanays.git@gmail.com>
2023-08-25 17:32:52 +0300

    updateargument check conditional

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,5 @@
 const sumAll = function(num1, num2) {
-    if(num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") {
-        // console.log(`num1 negative = ${num1 < 0} \n
-        // num2 negative = ${num2 < 0} \n
-        // num1 Nan = ${typeof num1 !== "number"} \n
-        // num2 Nan = ${typeof num2 !== "number"}`)
-
-        return 'ERROR';
-    }
+    if(num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") return 'ERROR';
 
     let sum = 0;
     let startNum = (num1 < num2) ? num1 : num2;
